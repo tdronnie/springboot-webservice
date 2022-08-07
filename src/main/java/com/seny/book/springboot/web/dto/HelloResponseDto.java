a2c3c4b026030e935ab5a9feaa40437d9baebbbf
seeunlee <seny9975@naver.com>
2022-08-07 23:21:32 +0900

    3장 완료

---
+++ src/main/java/com/seny/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.seny.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
