4fd718bdb46495198181b3d76ab703a60d0237c4
seeunlee <seny9975@naver.com>
2022-08-08 22:47:41 +0900

    게시글 등록/조회/수정/삭제 완료

--- src/main/java/com/seny/book/springboot/web/IndexController.java
+++ src/main/java/com/seny/book/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.seny.book.springboot.web;
 
 import com.seny.book.springboot.service.posts.PostsService;
+import com.seny.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,5 +26,10 @@ public class IndexController {
         return "posts-save";
     }
 
-
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
 }
