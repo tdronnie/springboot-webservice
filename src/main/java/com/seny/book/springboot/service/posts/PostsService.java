e9891d67082c394e26c02946b44e7bb36c7323a2
seeunlee <seny9975@naver.com>
2022-08-08 13:25:32 +0900

    게시글 등록, 조회 완료

--- src/main/java/com/seny/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/seny/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.seny.book.springboot.service.posts;
 
 import com.seny.book.springboot.domain.posts.Posts;
 import com.seny.book.springboot.domain.posts.PostsRepository;
+import com.seny.book.springboot.web.dto.PostsListResponseDto;
 import com.seny.book.springboot.web.dto.PostsResponseDto;
 import com.seny.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seny.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+    
+    @Transactional(readOnly = true) //트랜잭션 범위는 그대로 조회속도 향상
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
