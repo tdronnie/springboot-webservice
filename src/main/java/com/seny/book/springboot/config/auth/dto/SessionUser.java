5d342861e62b372422c4b5b396f13dd2524f3c25
seeunlee <seny9975@naver.com>
2022-08-10 03:00:49 +0900

    구글 로그인, 권한관리 완료

---
+++ src/main/java/com/seny/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.seny.book.springboot.config.auth.dto;
+
+import com.seny.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/seny/book/springboot/web/IndexController.java
+++ src/main/java/com/seny/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.seny.book.springboot.web;
 
+import com.seny.book.springboot.config.auth.dto.SessionUser;
 import com.seny.book.springboot.service.posts.PostsService;
 import com.seny.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
+
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
