6b4ed069b92ec366c455b0f4254032bd06426923
seeunlee <seny9975@naver.com>
2022-08-11 00:42:51 +0900

    세션기능 어노테이션 기반 개선 완료

--- src/main/java/com/seny/book/springboot/web/IndexController.java
+++ src/main/java/com/seny/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.seny.book.springboot.web;
 
+import com.seny.book.springboot.config.auth.LoginUser;
 import com.seny.book.springboot.config.auth.dto.SessionUser;
 import com.seny.book.springboot.service.posts.PostsService;
 import com.seny.book.springboot.web.dto.PostsResponseDto;
@@ -21,9 +22,9 @@ public class IndexController {
 
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
